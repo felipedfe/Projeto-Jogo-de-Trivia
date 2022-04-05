cf8c2d6aaced9ea28fc9cdc88c035d6e0ae43621
Davíd Roggér <davidrogger.a@gmail.com>
2022-04-05 16:12:50 -0300

    estrutura inicial do projeto

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ export default function App() {
       <header className="App-header">
         <img src={ logo } className="App-logo" alt="logo" />
         <p>
-          Primeira PR
+          Iniciando Login
         </p>
       </header>
     </div>
