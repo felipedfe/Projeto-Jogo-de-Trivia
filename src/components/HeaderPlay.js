8d1060bfd42a4ac9c45ab04a95ff210b9e3c887f
Davíd Roggér <davidrogger.a@gmail.com>
2022-04-06 07:22:08 -0300

    distribuido localizacao de cada informacao n componente

--- src/components/HeaderPlay.js
+++ src/components/HeaderPlay.js
@@ -1,17 +1,41 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
 class HeaderPlay extends Component {
   render() {
+    const { name, img, score } = this.props;
     return (
       <div className="headerplay-container">
-        avatar
-        nome
-        score
+        <img
+          data-testid="header-profile-picture"
+          src={ img }
+          alt={ `${name}-avatar` }
+        />
+        <h2>
+          { 'Player: '(
+            <span
+              data-testid="header-player-name"
+            >
+              {name}
+            </span>,
+          ) }
+        </h2>
+        <h1>
+          { 'Score: '(
+            <span>{score}</span>,
+          ) }
+        </h1>
       </div>
     );
   }
 }
+
+HeaderPlay.propTypes = {
+  name: PropTypes.string,
+  img: PropTypes.string,
+  score: PropTypes.number,
+}.isRequired;
 
 const mapStateToProps = (state) => ({
   name: state.player.name,
