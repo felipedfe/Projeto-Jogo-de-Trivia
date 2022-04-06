828a5c6b985f930146d0d4743700c58a1f2b0133
Davíd Roggér <davidrogger.a@gmail.com>
2022-04-06 13:13:45 -0300

    criado componente de questionmultiple

---
+++ src/components/QuestionMultiple.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class QuestionMultiple extends Component {
+  render() {
+    return (
+      <div className="question-container">
+        QUestion Multiple
+      </div>
+    );
+  }
+}
+
+export default QuestionMultiple;
