39b1fb802717f49ee73b77a589ba2f893878cb3d
Davíd Roggér <davidrogger.a@gmail.com>
2022-04-06 08:28:51 -0300

    criado pagina do gameboard

---
+++ src/pages/GameBoard.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import HeaderPlay from '../components/HeaderPlay';
+
+class GameBoard extends Component {
+  render() {
+    return (
+      <div className="gameboard-container">
+        <HeaderPlay />
+      </div>
+    );
+  }
+}
+
+export default GameBoard;
