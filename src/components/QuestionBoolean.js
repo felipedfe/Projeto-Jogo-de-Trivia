bae81fab990a768287850815fc83ce6676c7157a
Davíd Roggér <davidrogger.a@gmail.com>
2022-04-06 13:15:30 -0300

    criado componente de questionboolean

---
+++ src/components/QuestionBoolean.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class QuestionBoolean extends Component {
+  render() {
+    return (
+      <div className="question-container">
+        QUestion Boolean
+      </div>
+    );
+  }
+}
+
+export default QuestionBoolean;
