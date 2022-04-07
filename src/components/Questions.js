dbbc4b435e82c9b126de38244897501501c0c05c
Davíd Roggér <davidrogger.a@gmail.com>
2022-04-07 07:45:14 -0300

    limpando codigos comentados

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -1,55 +1,16 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-// import { fetchApiTrivia } from '../redux/actions';
-// import { fetchApiToken, actionGetQuestions } from '../redux/actions';
 import QuestionCard from './QuestionCard';
 
 class Questions extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      // loading: true,
       indexPosition: 0,
       // nextDisable: true,
     };
   }
-
-  // componentDidMount() {
-  //   this.gameStart();
-  // }
-
-  // fetchApiTrivia = async (token, quantity) => {
-  //   try {
-  //     const response = await fetch(`https://opentdb.com/api.php?amount=${quantity}&token=${token}`);
-  //     const data = await response.json();
-  //     return data;
-  //   } catch (error) {
-  //     console.log(`Erro encontrado Token API: ${error}`); // Provisório
-  //   }
-  // }
-
-  // gameStart = async () => {
-  //   const { saveQuestions } = this.props;
-  //   const questions = await this.getQuestions();
-  // console.log(questions);
-  // saveQuestions(questions);
-  // this.setState({
-  //   loading: false,
-  // });
-  // }
-
-  // getQuestions = async () => {
-  //   const { token, quantity, dispatch } = this.props;
-  //   const questions = await this.fetchApiTrivia(token, quantity);
-  //   const RESPONSE_CODE = 3;
-  //   if (questions.response_code === RESPONSE_CODE) {
-  //     await dispatch(fetchApiToken());
-  //     const retryQuestions = await this.fetchApiTrivia(token, quantity);
-  //     return retryQuestions;
-  //   }
-  //   return questions;
-  // }
 
   render() {
     const { indexPosition } = this.state;
@@ -64,14 +25,8 @@ class Questions extends Component {
 }
 
 const mapStateToProps = (state) => ({
-  // token: state.token,
-  // quantity: state.settings.quantity,
   questions: state.questions,
 });
-
-// const mapDispatchToProps = (dispatch) => ({
-//   saveQuestions: (questions) => dispatch(actionGetQuestions(questions)),
-// });
 
 Questions.propTypes = {
   dispatch: PropTypes.func,
