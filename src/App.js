9596028020565c1e83ee67a628cdcdd9bf4e3eb3
Felipe Fernandes <felipedfe@gmail.com>
2022-04-08 10:26:30 -0300

    requisito 16

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Login from './pages/Login';
 import GameBoard from './pages/GameBoard';
 import Settings from './pages/Settings';
 import Feedback from './pages/Feedback';
+import Ranking from './pages/Ranking';
 import './styles/general.css';
 
 class App extends Component {
@@ -15,6 +16,7 @@ class App extends Component {
         <Route exact path="/gameboard" component={ GameBoard } />
         <Route exact path="/settings" component={ Settings } />
         <Route exact path="/feedback" component={ Feedback } />
+        <Route exact path="/ranking" component={ Ranking } />
         <Route exact path="/" component={ Login } />
       </Switch>
     );
